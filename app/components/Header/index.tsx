a5a2d001383ebb3f99ff42b711a96df63a70fafb
Gyulo94 <qkrcjfrb112@gmail.com>
2024-08-09 17:48:38 +0900

    헤더 작성중

---
+++ app/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import "./style.css";
+export default function Header() {
+  return (
+    <header id="header-wrapper">
+      <div className="header-container">
+        <div className="logo-wrapper">
+          <img src="" alt="" />
+          <p>로고</p>
+        </div>
+        <div className="nav-wrapper">
+          <div className="nav-1">메뉴</div>
+          <div className="nav-2">메뉴</div>
+          <div className="nav-3">메뉴</div>
+          <div className="nav-4">메뉴</div>
+        </div>
+      </div>
+    </header>
+  );
+}
